3ddc088ae1c5742627abb7df6e084fab52d3559a
Adrien <adriendellagaspera@outlook.com>
2023-10-21 07:22:03 +0200

    Impl RangeComparable for any PartialOrd

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeBounds;
+use std::ops::{Bound, RangeBounds};
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum RangeOrdering {
@@ -11,3 +11,25 @@ pub trait RangeComparable {
     type T: PartialOrd;
     fn range_cmp<R: RangeBounds<Self::T>>(&self, range: &R) -> RangeOrdering;
 }
+
+impl<T: PartialOrd> RangeComparable for T {
+    type T = T;
+    fn range_cmp<R: RangeBounds<T>>(&self, range: &R) -> RangeOrdering {
+        if match range.start_bound() {
+            Bound::Included(key) => self < key,
+            Bound::Excluded(key) => self <= key,
+            _ => false,
+        } {
+            return RangeOrdering::Below;
+        }
+        if match range.end_bound() {
+            Bound::Included(key) => self > key,
+            Bound::Excluded(key) => self >= key,
+            _ => false,
+        } {
+            return RangeOrdering::Above;
+        }
+        RangeOrdering::Inside
+    }
+}
+

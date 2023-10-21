09fe74690258a7cab8fae5cc511075354c25e28e
Adrien <adriendellagaspera@outlook.com>
2023-10-21 07:20:56 +0200

    Introduce trait RangeComparable

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+use std::ops::RangeBounds;
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum RangeOrdering {
     Below,
     Inside,
     Above,
+}
+
+pub trait RangeComparable {
+    type T: PartialOrd;
+    fn range_cmp<R: RangeBounds<Self::T>>(&self, range: &R) -> RangeOrdering;
 }

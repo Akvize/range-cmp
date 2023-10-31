a7b4009d725f38817046a423f811c65f71e5bdbb
Quentin Santos <qsantos@qsantos.fr>
2023-10-31 13:29:27 +0100

    Add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// Copyright 2023 Developers of the range_cmp project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 //! This crate provides the [`RangeComparable`] trait on all types that implement [`PartialOrd`].
 //! This traits exposes a [`range_cmp`](RangeComparable::range_cmp) associated method that allows
 //! comparing a value with a range of values:

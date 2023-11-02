5de6460c8ae66150bb0074f037e0e2c70bfe24b7
Adrien <adriendellagaspera@outlook.com>
2023-11-03 00:25:28 +0100

    Introduce PartialRangeOrd

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,16 @@
 //! assert_eq!(25.rcmp(20..30), RangeOrdering::Inside);
 //! assert_eq!(35.rcmp(20..30), RangeOrdering::Above);
 //! ```
+//!
+//! This crates also provides the [`PartialRangeOrd`] trait on all types that implement [`PartialOrd`].
+//! This traits exposes a [`partial_rcmp`](PartialRangeOrd::partial_rcmp) associated method:
+//!
+//! ```
+//! use range_cmp::{PartialRangeOrd, RangeOrdering};
+//! assert_eq!(15.partial_rcmp(20..30), Some(RangeOrdering::Below));
+//! assert_eq!(25.partial_rcmp(20..30), Some(RangeOrdering::Inside));
+//! assert_eq!(35.partial_rcmp(20..30), Some(RangeOrdering::Above));
+//! ```
 
 use std::borrow::Borrow;
 use std::ops::{Bound, RangeBounds};
@@ -129,8 +139,58 @@ impl<T: Ord> RangeOrd for T {
     }
 }
 
+/// Trait to provide the [`partial_rcmp`](PartialRangeOrd::partial_rcmp) method, which allows comparing
+/// the type to a range. A blanket implementation is provided for all types that implement the
+/// [`PartialOrd`] trait.
+pub trait PartialRangeOrd {
+    /// Compare the value to a range of values. Returns whether the value is below, inside or above
+    /// the range.
+    ///
+    /// ```
+    /// use range_cmp::{PartialRangeOrd, RangeOrdering};
+    /// assert_eq!(15.partial_rcmp(20..30), Some(RangeOrdering::Below));
+    /// assert_eq!(25.partial_rcmp(20..30), Some(RangeOrdering::Inside));
+    /// assert_eq!(35.partial_rcmp(20..30), Some(RangeOrdering::Above));
+    /// ```
+    fn partial_rcmp<R: RangeBounds<Self>, B: BorrowRange<Self, R>>(
+        &self,
+        range: B,
+    ) -> Option<RangeOrdering>;
+}
+
+impl<T: PartialOrd> PartialRangeOrd for T {
+    fn partial_rcmp<R: RangeBounds<Self>, B: BorrowRange<Self, R>>(
+        &self,
+        range: B,
+    ) -> Option<RangeOrdering> {
+        let range = range.borrow();
+
+        if range.contains(self) {
+            return Some(RangeOrdering::Inside);
+        }
+
+        if match range.start_bound() {
+            Bound::Included(key) => self < key,
+            Bound::Excluded(key) => self <= key,
+            _ => false,
+        } {
+            return Some(RangeOrdering::Below);
+        }
+
+        if match range.end_bound() {
+            Bound::Included(key) => self > key,
+            Bound::Excluded(key) => self >= key,
+            _ => false,
+        } {
+            return Some(RangeOrdering::Above);
+        }
+
+        None
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod rcmp_tests {
     use super::*;
 
     #[test]
@@ -356,5 +416,235 @@ mod tests {
         // 30 is above [25, 15[
         assert_eq!(30.rcmp(25..15), RangeOrdering::Above);
         assert_eq!(30.rcmp(&25..&15), RangeOrdering::Above);
+    }
+}
+
+#[cfg(test)]
+mod partial_rcmp_tests {
+    use super::*;
+
+    #[test]
+    fn range_full() {
+        // 1 is inside ]-inf, inf[
+        assert_eq!(1.partial_rcmp(..), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn range_from() {
+        // 1 is inside [1, +inf[
+        assert_eq!(1.partial_rcmp(1..), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(&1..), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, +inf[
+        assert_eq!(1.partial_rcmp(2..), Some(RangeOrdering::Below));
+        assert_eq!(1.partial_rcmp(&2..), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn range_to() {
+        // 1 is above ]-inf, 1[
+        assert_eq!(1.partial_rcmp(..1), Some(RangeOrdering::Above));
+        assert_eq!(1.partial_rcmp(..&1), Some(RangeOrdering::Above));
+
+        // 1 is inside ]-inf, 2[
+        assert_eq!(1.partial_rcmp(..2), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(..&2), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn range() {
+        // 1 is above [0, 1[
+        assert_eq!(1.partial_rcmp(0..1), Some(RangeOrdering::Above));
+        assert_eq!(1.partial_rcmp(&0..&1), Some(RangeOrdering::Above));
+
+        // 1 is inside [1, 2[
+        assert_eq!(1.partial_rcmp(1..2), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(&1..&2), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, 3[
+        assert_eq!(1.partial_rcmp(2..3), Some(RangeOrdering::Below));
+        assert_eq!(1.partial_rcmp(&2..&3), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn range_inclusive() {
+        // 1 is above [0, 0]
+        assert_eq!(1.partial_rcmp(0..=0), Some(RangeOrdering::Above));
+        assert_eq!(1.partial_rcmp(&0..=&0), Some(RangeOrdering::Above));
+
+        // 1 is inside [1, 1]
+        assert_eq!(1.partial_rcmp(1..=1), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(&1..=&1), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, 2]
+        assert_eq!(1.partial_rcmp(2..=2), Some(RangeOrdering::Below));
+        assert_eq!(1.partial_rcmp(&2..=&2), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn range_to_inclusive() {
+        // 1 is above ]-inf, 0]
+        assert_eq!(1.partial_rcmp(..=0), Some(RangeOrdering::Above));
+        assert_eq!(1.partial_rcmp(..=&0), Some(RangeOrdering::Above));
+
+        // 1 is inside ]-inf, 1
+        assert_eq!(1.partial_rcmp(..=1), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(..=&1), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn bounds_full() {
+        // 1 is inside ]-inf, inf[
+        let bounds: (Bound<i32>, Bound<i32>) = (Bound::Unbounded, Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn bounds_from() {
+        // 1 is inside [1, +inf[
+        let bounds = (Bound::Included(1), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds = (Bound::Included(&1), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, +inf[
+        let bounds = (Bound::Included(2), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+
+        let bounds = (Bound::Included(&2), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn bounds_to() {
+        // 1 is above ]-inf, 1[
+        let bounds = (Bound::Included(1), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds = (Bound::Included(&1), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        // 1 is inside ]-inf, 2[
+        let bounds = (Bound::Included(2), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+
+        let bounds = (Bound::Included(&2), Bound::Unbounded);
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn bounds() {
+        // 1 is above [0, 1[
+        let bounds = (Bound::Included(0), Bound::Excluded(1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        let bounds = (Bound::Included(&0), Bound::Excluded(&1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        // 1 is inside [1, 2[
+        let bounds = (Bound::Included(1), Bound::Excluded(2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds = (Bound::Included(&1), Bound::Excluded(&2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, 3[
+        let bounds = (Bound::Included(2), Bound::Excluded(3));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+
+        let bounds = (Bound::Included(&2), Bound::Excluded(&3));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn bounds_inclusive() {
+        // 1 is above [0, 0]
+        let bounds = (Bound::Included(0), Bound::Included(0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        let bounds = (Bound::Included(&0), Bound::Included(&0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        // 1 is inside [1, 1]
+        let bounds = (Bound::Included(1), Bound::Included(1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds = (Bound::Included(&1), Bound::Included(&1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        // 1 is below [2, 2]
+        let bounds = (Bound::Included(2), Bound::Included(2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+
+        let bounds = (Bound::Included(&2), Bound::Included(&2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn bounds_to_inclusive() {
+        // 1 is above ]-inf, 0]
+        let bounds = (Bound::Unbounded, Bound::Included(0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        let bounds = (Bound::Unbounded, Bound::Included(&0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        // 1 is inside ]-inf, 1]
+        let bounds = (Bound::Unbounded, Bound::Included(1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds = (Bound::Unbounded, Bound::Included(&1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn bounds_exclusive_inclusive() {
+        // 1 is above ]-1, 0]
+        let bounds: (Bound<i32>, Bound<i32>) = (Bound::Excluded(-1), Bound::Included(0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        let bounds: (Bound<&i32>, Bound<&i32>) = (Bound::Excluded(&-1), Bound::Included(&0));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Above));
+
+        // 1 is inside ]0, 1]
+        let bounds: (Bound<i32>, Bound<i32>) = (Bound::Excluded(0), Bound::Included(1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        let bounds: (Bound<&i32>, Bound<&i32>) = (Bound::Excluded(&0), Bound::Included(&1));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+
+        // 1 is below ]1, 2]
+        let bounds: (Bound<i32>, Bound<i32>) = (Bound::Excluded(1), Bound::Included(2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+
+        let bounds: (Bound<&i32>, Bound<&i32>) = (Bound::Excluded(&1), Bound::Included(&2));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Below));
+    }
+
+    #[test]
+    fn bounds_as_reference() {
+        let bounds = 0..2;
+        assert_eq!(1.partial_rcmp(&bounds), Some(RangeOrdering::Inside));
+        assert_eq!(1.partial_rcmp(bounds), Some(RangeOrdering::Inside));
+    }
+
+    #[test]
+    fn empty_ranges() {
+        // 0 is above [0, 0[
+        assert_eq!(0.partial_rcmp(0..0), Some(RangeOrdering::Above));
+        assert_eq!(0.partial_rcmp(&0..&0), Some(RangeOrdering::Above));
+
+        // 0u32 is above [-inf, 0u32[
+        assert_eq!(0.partial_rcmp(..0u32), Some(RangeOrdering::Above));
+        assert_eq!(0.partial_rcmp(..&0u32), Some(RangeOrdering::Above));
+
+        // 30 is below [45, 35[
+        assert_eq!(30.partial_rcmp(45..35), Some(RangeOrdering::Below));
+        assert_eq!(30.partial_rcmp(&45..&35), Some(RangeOrdering::Below));
+
+        // 30 is above [25, 15[
+        assert_eq!(30.partial_rcmp(25..15), Some(RangeOrdering::Above));
+        assert_eq!(30.partial_rcmp(&25..&15), Some(RangeOrdering::Above));
     }
 }
